2cf4522a93a170433945d49de5633f1e6f8f532a
Saikat-D023 <saikatdey358@gmail.com>
2024-12-08 19:38:26 +0530

    recoil's atom feature discussed and added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,59 @@
 import { useState } from 'react'
 import './App.css'
+import { RecoilRoot, useSetRecoilState, atom, useRecoilValue } from 'recoil'
+import { counterAtom } from './store/atoms/counter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <RecoilRoot>
+      <Counter />
+    </RecoilRoot>
   )
+}
+
+function Counter(){
+
+    return <div>
+      <CurrentCount />
+      <Increase />
+      <Decrease />
+    </div>
+}
+
+function CurrentCount(){
+  //this comp has now subscribed to the value of the atom
+   const count = useRecoilValue(counterAtom) 
+    
+   return <div>
+    {count}
+   </div> 
+}
+
+function Decrease(){
+  //this comp is subscribed to the setter
+  const setCount = useSetRecoilState(counterAtom)
+
+  function decrease(){
+    setCount(c => c - 1)
+  }
+
+  return <div>
+    <button onClick={decrease}>Decrease</button>
+  </div>
+}
+
+function Increase(){
+  //this comp is also subscribed to the setter of the atom
+  const setCount = useSetRecoilState(counterAtom)
+
+  function increase(){
+    setCount(c => c + 1)
+  }
+
+  return <div>
+    <button onClick={increase}>Increase</button>
+  </div>
 }
 
 export default App
